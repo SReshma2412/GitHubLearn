3bcde2123927fc4b18ef5b2b0fb1d2d6e6ea6404
SReshma2412 <144695817+SReshma2412@users.noreply.github.com>
2025-01-11 22:16:38 +0530

    Update AddTwoNum.java
    Update variable b

--- AddTwoNum.java
+++ AddTwoNum.java
@@ -1,7 +1,7 @@
 public class AddTwoNum{
     public static void main(String[]args) {
         int a=20;
-        int b=20;
+        int b=10;
         int c=a+b;
         System.out.println(c);
     }
